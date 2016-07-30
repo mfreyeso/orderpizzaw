71c9de71b0e3d59d4336a8996d427be346348885
Mario Reyes Ojeda <mfreyesojeda@gmail.com>
2016-07-30 12:42:10 -0500

    Initial Commit

---
+++ OrderPizzaW WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  OrderPizzaW WatchKit Extension
+//
+//  Created by Mario on 30/07/16.
+//  Copyright © 2016 Mario. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
