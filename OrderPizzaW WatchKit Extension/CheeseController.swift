a29b22c190578b579ca986010fba4e5dbe79bea5
Mario Reyes Ojeda <mfreyesojeda@gmail.com>
2016-07-30 13:09:27 -0500

    Add all views for app, create all interfaces controllers, name and bind between these.

---
+++ OrderPizzaW WatchKit Extension/CheeseController.swift
@@ -0,0 +1,31 @@
+//
+//  CheeseController.swift
+//  OrderPizzaW
+//
+//  Created by Mario on 30/07/16.
+//  Copyright © 2016 Mario. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class CheeseController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
